7a5a918a6b23a8f08eb0ed331b30f0f8b7ddc3cc
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-15 17:20:44 -0500

    chore: add application resources, i18n messages, and smoke test

---
+++ src/test/java/com/devteam/nutrismart/platform/NutrismartPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devteam.nutrismart.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrismartPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
