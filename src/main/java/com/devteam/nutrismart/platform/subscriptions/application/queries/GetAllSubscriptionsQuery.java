c6aa26db2cde387f32559e9ecd366fa578548c07
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-16 00:22:57 -0500

    feat(subscriptions): add GetAllSubscriptionsQuery

---
+++ src/main/java/com/devteam/nutrismart/platform/subscriptions/application/queries/GetAllSubscriptionsQuery.java
@@ -0,0 +1,7 @@
+package com.devteam.nutrismart.platform.subscriptions.application.queries;
+
+/**
+ * Consulta para recuperar todas las suscripciones registradas en el sistema,
+ * sin aplicar ningún filtro. Utilizada principalmente en contextos administrativos.
+ */
+public record GetAllSubscriptionsQuery() {}
