27435b5cae243b8adebab2dddcd8f1b7f11e855b
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-16 00:22:57 -0500

    feat(subscriptions): add GetAllBillingRecordsQuery

---
+++ src/main/java/com/devteam/nutrismart/platform/subscriptions/application/queries/GetAllBillingRecordsQuery.java
@@ -0,0 +1,7 @@
+package com.devteam.nutrismart.platform.subscriptions.application.queries;
+
+/**
+ * Consulta para recuperar todos los registros de pago del sistema,
+ * sin aplicar ningún filtro. Utilizada principalmente en contextos administrativos.
+ */
+public record GetAllBillingRecordsQuery() {}
