4a90ef3cdedf2bc5652031a99af03f966e8a9e7a
Brandon <brandon7ph@gmail.com>
2026-06-15 19:42:31 -0500

    feat: add FoodRestriction enum
    Introduce FoodRestriction enum in the nutritiontracking domain to represent common dietary restrictions (LACTOSE_FREE, GLUTEN_FREE, VEGAN, VEGETARIAN, NUT_FREE, SEAFOOD_FREE, KOSHER, HALAL). This value object centralizes permitted restriction types for use across the nutrition tracking model.

---
+++ src/main/java/com/devteam/nutrismart/platform/nutritiontracking/domain/model/valueobjects/FoodRestriction.java
@@ -0,0 +1,12 @@
+package com.devteam.nutrismart.platform.nutritiontracking.domain.model.valueobjects;
+
+public enum FoodRestriction {
+    LACTOSE_FREE,
+    GLUTEN_FREE,
+    VEGAN,
+    VEGETARIAN,
+    NUT_FREE,
+    SEAFOOD_FREE,
+    KOSHER,
+    HALAL
+}
